5e1f646eefc4c2862290927d74a3ce97a2b5082a
LapkaQ <wojtekgaw2007@gmail.com>
2024-06-22 21:04:59 +0200

    trying v2

--- app/components/GoogleAnalytics.js
+++ app/components/GoogleAnalytics.js
@@ -8,12 +8,12 @@ const GoogleAnalytics = () => {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       ></Script>
-      <Script>
-        {`  window.dataLayer = window.dataLayer || [];
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', ${process.env.NEXT_PUBLIC_MEASUREMENT_ID});`}
+        gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');`}
       </Script>
     </>
   );
