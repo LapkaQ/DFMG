8e0c36f9a317d4941426950135ab65b23c7c0d76
LapkaQ <wojtekgaw2007@gmail.com>
2024-06-22 21:03:12 +0200

    im trying to fix googleAnalytics sadek

--- app/components/GoogleAnalytics.js
+++ app/components/GoogleAnalytics.js
@@ -5,19 +5,15 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        strategy="lazyOnload"
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      />
+      ></Script>
+      <Script>
+        {`  window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-      <Script id="" strategy="lazyOnload">
-        {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-                });
-            `}
+        gtag('config', ${process.env.NEXT_PUBLIC_MEASUREMENT_ID});`}
       </Script>
     </>
   );
