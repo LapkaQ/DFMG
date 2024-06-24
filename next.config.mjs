ed73aa13e1dc37e8e03fcd54fbcc833d5321514d
LapkaQ <wojtekgaw2007@gmail.com>
2024-06-24 11:55:11 +0200

    Add CORS headers in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
     domains: ["cdn.discordapp.com"],
   },
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/app/fonts/:all*", // Ścieżka do plików, dla których chcesz dodać nagłówki
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Możesz ograniczyć do konkretnego źródła np. 'https://www.dcfaker.xyz'
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
